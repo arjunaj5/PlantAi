dbef4f1ef884b0e762acd0f196b46fadb038b9f4
Ayana <ayanapreman2001@gmail.com>
2022-02-01 22:41:12 +0530

    disease result page

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect,useState} from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-// Components
+//Components
 import LoginOrSignup from './Screens/LoginOrSignup';
 import Home from './Screens/Home';
+import Result from './Screens/PlantResult';
+
+
+
+
+
 
 export default function App() {
+ 
   return (
+   
+
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerShown: false
         }}
       >
+         <Stack.Screen
+      name="PlantResult"
+      component={Result}
+      />
+
         <Stack.Screen
           name="LoginSignup"
           component={LoginOrSignup}
@@ -29,13 +43,15 @@ export default function App() {
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
+   
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#D3FFD8',
+   
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Screens/PlantResult/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:"space-around",
+        flexDirection:"column",
+        alignItems:"center",
+        paddingHorizontal:10,
+        width:'90%',
+        height:'90%',
+        backgroundColor:'white',
+        borderBottomRightRadius:19,
+        borderBottomLeftRadius:19
+
+    },
+  Image: {
+    width: '90%',
+    height:200
+    
+  },
+  
+    Text: {
+        fontSize: 18,
+        marginTop: 28,
+        fontWeight:'bold',
+        alignSelf:"center"
+      },
+    
+    
+  Button: {
+    width: 149,
+    height:40,
+     borderRadius: 22,
+    marginVertical: 9,
+    paddingVertical:2,
+    alignSelf:"center"
+    }
+})
+
+export default styles
